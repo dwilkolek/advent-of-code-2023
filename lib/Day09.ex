939e520aa988818bdace50a5f251bdc753d91aa1
Damian Wilkolek <dwilkolek@protonmail.com>
2023-12-09 15:33:29 +0100

    day 09 - part 2

--- lib/Day09.ex
+++ lib/Day09.ex
@@ -1,20 +1,23 @@
 defmodule Day09 do
   def solve1(input) do
     parse(input)
-    |> Enum.map(fn line -> find_line_below(line) end)
+    |> Enum.map(fn line -> find_line_below_rhs(line) end)
     |> Enum.sum()
   end
 
   def solve2(input) do
+    parse(input)
+    |> Enum.map(fn line -> find_line_below_lhs(line) end)
+    |> Enum.sum()
   end
 
-  def find_line_below(line) do
+  def find_line_below_rhs(line) do
     last = hd(Enum.reverse(line))
-    res = find_line_below(line, [])
+    res = find_line_below_rhs(line, [])
     hd(res) + last
   end
 
-  def find_line_below(line, acc) do
+  def find_line_below_rhs(line, acc) do
     result =
       line
       |> Enum.chunk_every(2, 1, :discard)
@@ -25,7 +28,7 @@ defmodule Day09 do
     if result == zeros do
       [0]
     else
-      sub_result = find_line_below(result, [result | acc])
+      sub_result = find_line_below_rhs(result, [result | acc])
       from_row_below = hd(sub_result)
 
       last_here = hd(Enum.reverse(result))
@@ -34,11 +37,35 @@ defmodule Day09 do
     end
   end
 
+  def find_line_below_lhs(line) do
+    first = hd(line)
+    res = find_line_below_lhs(line, [])
+    first - hd(res)
+  end
+
+  def find_line_below_lhs(line, acc) do
+    result =
+      line
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.map(fn [a, b] -> b - a end)
+
+    zeros = List.duplicate(0, length(result))
+
+    if result == zeros do
+      [0]
+    else
+      sub_result = find_line_below_lhs(result, [result | acc])
+      from_row_below = hd(sub_result)
+
+      first_here = hd(result)
+      [first_here - from_row_below | sub_result]
+    end
+  end
+
   def parse(input) do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(fn line -> String.split(line, " ") |> Enum.map(&String.to_integer/1) end)
-    |> IO.inspect()
   end
 end
 
@@ -46,7 +73,7 @@ defmodule Mix.Tasks.Day09 do
   use Mix.Task
 
   def run(_) do
-    {:ok, input} = File.read("inputs/09-test1.txt")
+    {:ok, input} = File.read("inputs/09.txt")
     IO.puts("--- Part 1 ---")
     IO.puts(Day09.solve1(input))
     IO.puts("")
