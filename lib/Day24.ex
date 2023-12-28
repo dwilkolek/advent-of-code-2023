645dffeeca065da97fdf44d15b1a4cfa8dde1d2d
Damian Wilkolek <dwilkolek@protonmail.com>
2023-12-28 09:45:13 +0100

    day 24 - part 2

--- lib/Day24.ex
+++ lib/Day24.ex
@@ -1,4 +1,7 @@
 defmodule Day24 do
+  @min 200_000_000_000_000
+  @max 400_000_000_000_000
+
   def solve1(input) do
     mat =
       parse(input)
@@ -19,36 +22,151 @@ defmodule Day24 do
     end)
     |> List.flatten()
     |> Enum.filter(fn m ->
-      min = 200000000000000
-      max = 400000000000000
-      # IO.inspect(m)
-
-
       case m do
-        nil -> false
+        nil ->
+          false
+
         {x, y, {{xa, ya, _, xad, yad, _}, {xb, yb, _, xbd, ybd, _}}} ->
           a_in_right_dir_y = if yad >= 0, do: y >= ya, else: y <= ya
           b_in_right_dir_y = if ybd >= 0, do: y >= yb, else: y <= yb
           a_in_right_dir_x = if xad >= 0, do: x >= xa, else: x <= xa
           b_in_right_dir_x = if xbd >= 0, do: x >= xb, else: x <= xb
-          in_area = x >= min && x <= max && y >= min && y <= max
-          # IO.puts("Reason: #{in_area}, #{a_in_right_dir_y}, #{b_in_right_dir_y}, #{a_in_right_dir_x}, #{b_in_right_dir_x}")
+          in_area = x >= @min && x <= @max && y >= @min && y <= @max
+
           in_area && b_in_right_dir_x && a_in_right_dir_y && b_in_right_dir_y && a_in_right_dir_x
       end
     end)
-    # |> IO.inspect()
     |> Enum.count()
   end
 
-  def solve2(_input) do
+  def solve2(input) do
+    mat =
+      parse(input)
+
+    {px0, py0, pz0, vx0, vy0, vz0} = Enum.at(mat, 0)
+    {px1, py1, pz1, vx1, vy1, vz1} = Enum.at(mat, 1)
+    {px2, py2, _, vx2, vy2, _} = Enum.at(mat, 2)
+    {px3, py3, _, vx3, vy3, _} = Enum.at(mat, 3)
+    {px4, py4, _, vx4, vy4, _} = Enum.at(mat, 4)
+    {px5, py5, _, vx5, vy5, _} = Enum.at(mat, 5)
+    {px6, py6, _, vx6, vy6, _} = Enum.at(mat, 6)
+    {px7, py7, _, vx7, vy7, _} = Enum.at(mat, 7)
+
+    a = [
+      vy0 - vy1,
+      vx1 - vx0,
+      py1 - py0,
+      px0 - px1,
+      vy2 - vy3,
+      vx3 - vx2,
+      py3 - py2,
+      px2 - px3,
+      vy4 - vy5,
+      vx5 - vx4,
+      py5 - py4,
+      px4 - px5,
+      vy6 - vy7,
+      vx7 - vx6,
+      py7 - py6,
+      px6 - px7
+    ]
+
+    b = [
+      px0 * vy0 - py0 * vx0 + py1 * vx1 - px1 * vy1,
+      px2 * vy2 - py2 * vx2 + py3 * vx3 - px3 * vy3,
+      px4 * vy4 - py4 * vx4 + py5 * vx5 - px5 * vy5,
+      px6 * vy6 - py6 * vx6 + py7 * vx7 - px7 * vy7
+    ]
+
+    den = det4x4(a)
+
+    pxr =
+      det4x4([
+        Enum.at(b, 0),
+        Enum.at(a, 1),
+        Enum.at(a, 2),
+        Enum.at(a, 3),
+        Enum.at(b, 1),
+        Enum.at(a, 5),
+        Enum.at(a, 6),
+        Enum.at(a, 7),
+        Enum.at(b, 2),
+        Enum.at(a, 9),
+        Enum.at(a, 10),
+        Enum.at(a, 11),
+        Enum.at(b, 3),
+        Enum.at(a, 13),
+        Enum.at(a, 14),
+        Enum.at(a, 15)
+      ]) / den
+
+    pyr =
+      det4x4([
+        Enum.at(a, 0),
+        Enum.at(b, 0),
+        Enum.at(a, 2),
+        Enum.at(a, 3),
+        Enum.at(a, 4),
+        Enum.at(b, 1),
+        Enum.at(a, 6),
+        Enum.at(a, 7),
+        Enum.at(a, 8),
+        Enum.at(b, 2),
+        Enum.at(a, 10),
+        Enum.at(a, 11),
+        Enum.at(a, 12),
+        Enum.at(b, 3),
+        Enum.at(a, 14),
+        Enum.at(a, 15)
+      ]) / den
+
+    vxr =
+      det4x4([
+        Enum.at(a, 0),
+        Enum.at(a, 1),
+        Enum.at(b, 0),
+        Enum.at(a, 3),
+        Enum.at(a, 4),
+        Enum.at(a, 5),
+        Enum.at(b, 1),
+        Enum.at(a, 7),
+        Enum.at(a, 8),
+        Enum.at(a, 9),
+        Enum.at(b, 2),
+        Enum.at(a, 11),
+        Enum.at(a, 12),
+        Enum.at(a, 13),
+        Enum.at(b, 3),
+        Enum.at(a, 15)
+      ]) / den
+
+    t0 = (pxr - px0) / (vx0 - vxr)
+    t1 = (pxr - px1) / (vx1 - vxr)
+    vzr = (pz0 - pz1 + t0 * vz0 - t1 * vz1) / (t0 - t1)
+    pzr = pz0 + t0 * (vz0 - vzr)
+
+    pxr + pyr + pzr
+  end
+
+  def det3x3(m) do
+    [m0, m1, m2, m3, m4, m5, m6, m7, m8] = m
+
+    m0 * m4 * m8 + m1 * m5 * m6 + m2 * m3 * m7 -
+      m0 * m5 * m7 - m1 * m3 * m8 - m2 * m4 * m6
+  end
+
+  def det4x4(m) do
+    [m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15] = m
+
+    m0 * det3x3([m5, m6, m7, m9, m10, m11, m13, m14, m15]) -
+      m1 * det3x3([m4, m6, m7, m8, m10, m11, m12, m14, m15]) +
+      m2 * det3x3([m4, m5, m7, m8, m9, m11, m12, m13, m15]) -
+      m3 * det3x3([m4, m5, m6, m8, m9, m10, m12, m13, m14])
   end
 
   def cross_at(a, b) do
     {xa, ya, ea, oa} = a
     {xb, yb, eb, ob} = b
-    # IO.puts("Cross of")
-    # IO.inspect(a)
-    # IO.inspect(b)
 
     det = xa * yb - xb * ya
 
@@ -58,9 +176,6 @@ defmodule Day24 do
           [yb / det, -1 * ya / det],
           [-1 * xb / det, xa / det]
         ]
-
-      # IO.puts("adt=")
-      # IO.inspect(adt)
 
       b = [ea, eb]
 
